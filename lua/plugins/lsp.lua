99e86252bd7cba8fa9eb3a858a9a5a3c98ea625e
daniel fusser <daniel.fusser@gmail.com>
2024-03-10 14:30:51 +0100

    add ray-x go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,17 @@ end)
 add 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
 now(function()
+  add 'ray-x/go.nvim'
+  require('go').setup()
+  local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+      require('go.format').goimport()
+    end,
+    group = format_sync_grp,
+  })
+
   add 'lewis6991/gitsigns.nvim'
   require('gitsigns').setup {
     signs = {
