e79c1a91c0a9b4fd7b82ade6510016daa6d474dc
daniel fusser <daniel.fusser@gmail.com>
2024-03-28 23:57:37 +0100

    add minidiff, various small fixes

--- init.lua
+++ init.lua
@@ -4,12 +4,12 @@
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
-vim.keymap.set('n', '<Space>', '<Nop>', { silent = true, remap = false })
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.o.grepprg = [[rg --vimgrep --hidden -g "!.git"]]
-vim.fn.setenv('LANG', 'en_US.UTF-8')
+vim.fn.setenv("LANG", "en_US.UTF-8")
 --
 
 -- Sets how neovim will display certain whitespace in the editor.
@@ -18,7 +18,7 @@ vim.fn.setenv('LANG', 'en_US.UTF-8')
 
 -- overrides for mini basics
 -- vim.opt.shortmess:append 'I' -- skip Intro
-vim.o.backspace = 'indent,eol,start' -- allow backspace
+vim.o.backspace = "indent,eol,start" -- allow backspace
 vim.o.whichwrap = [[b,s,<,>,[,]] -- allow keys to cross eol
 vim.o.expandtab = true
 -- vim.o.smarttab=true
@@ -30,15 +30,15 @@ vim.o.cursorline = false
 
 vim.o.clipboard = [[unnamed,unnamedplus]]
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
 
 -- Make line numbers default
 vim.opt.relativenumber = true
 -- vim.opt.number = true
 
-require 'dafu.keys'
+require("dafu.keys")
 -- require 'dafu.opts'
-require 'dafu.cmds'
-require 'dafu.timer'
+require("dafu.cmds")
+require("dafu.timer")
 
-require 'plugins'
+require("plugins")
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,69 +1,93 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 now(function()
-  require('mini.basics').setup {
-    options = {
-      basic = true,
-      extra_ui = true,
-      win_borders = 'solid',
-    },
-    mappings = {
-      windows = true,
-    },
-    autocommands = {
-      basic = true,
-      relnum_in_visual_mode = true,
-    },
-  }
-  require('mini.hues').setup {
-    background = '#2e3440',
-    foreground = '#eceff4',
-    accent = 'bg',
-    hues = 2,
-    plugins = {
-      default = false,
-      ['echasnovski/mini.nvim'] = true,
-      ['folke/trouble.nvim'] = true,
-      ['hrsh7th/nvim-cmp'] = true,
-      ['lewis6991/gitsigns.nvim'] = true,
-      ['williamboman/mason.nvim'] = true,
-    },
-  } -- nord
+	require("mini.basics").setup({
+		options = {
+			basic = true,
+			extra_ui = false,
+			win_borders = "single",
+		},
+		mappings = {
+			windows = true,
+		},
+		autocommands = {
+			basic = true,
+			relnum_in_visual_mode = true,
+		},
+	})
+	require("mini.hues").setup({
+		background = "#2e3440",
+		foreground = "#eceff4",
 
-  require('mini.notify').setup()
-  vim.notify = require('mini.notify').make_notify()
+		accent = "bg",
+		-- saturation = "low",
+		n_hues = 4,
+		plugins = {
+			default = true,
+			-- ["echasnovski/mini.nvim"] = true,
+			-- ["folke/trouble.nvim"] = true,
+			-- ["hrsh7th/nvim-cmp"] = true,
+			-- ["lewis6991/gitsigns.nvim"] = true,
+			-- ["williamboman/mason.nvim"] = true,
+		},
+	}) -- nord
 
-  require('mini.tabline').setup()
-  require('mini.files').setup()
+	require("mini.notify").setup()
+	vim.notify = require("mini.notify").make_notify()
 
-  vim.o.cmdheight = 0
-  vim.o.laststatus = 3
-  require('mini.statusline').setup {
-    -- Whether to set Vim's settings for statusline (make it always shown with
-    -- 'laststatus' set to 2). To use global statusline in Neovim>=0.7.0, set
-    -- this to `false` and 'laststatus' to 3.
-    set_vim_settings = false,
-  }
-  require('mini.starter').setup()
-  require('mini.ai').setup()
-  require('mini.comment').setup()
-  require('mini.pick').setup()
-  require('mini.surround').setup()
-  require('mini.bufremove').setup()
-  require('mini.indentscope').setup()
-  require('mini.visits').setup()
+	require("mini.tabline").setup()
+	require("mini.files").setup()
 
-  local hipatterns = require 'mini.hipatterns'
-  hipatterns.setup {
-    highlighters = {
-      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-      fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-      hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-      todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-      note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+	-- vim.o.cmdheight = 0
+	-- vim.o.laststatus = 3
+	require("mini.statusline").setup({
+		-- Whether to set Vim's settings for statusline (make it always shown with
+		-- 'laststatus' set to 2). To use global statusline in Neovim>=0.7.0, set
+		-- this to `false` and 'laststatus' to 3.
+		-- set_vim_settings = false,
+		content = {
+			active = function()
+				local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+				local git = MiniStatusline.section_git({ trunc_width = 75 })
+				local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+				local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+				local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+				local location = MiniStatusline.section_location({ trunc_width = 75 })
+				local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
-      -- Highlight hex color strings (`#rrggbb`) using that color
-      hex_color = hipatterns.gen_highlighter.hex_color(),
-    },
-  }
+				return MiniStatusline.combine_groups({
+					{ hl = mode_hl, strings = { mode } },
+					{ hl = "MiniStatuslineFilename", strings = { git, diagnostics } },
+					"%<", -- Mark general truncate point
+					{ hl = "MiniStatuslineFilename", strings = { filename } },
+					"%=", -- End left alignment
+					{ hl = "MiniStatuslineFilename", strings = { fileinfo } },
+					{ hl = "MiniStatuslineFilename", strings = { search, location } },
+				})
+			end,
+		},
+	})
+	require("mini.starter").setup()
+	require("mini.ai").setup()
+	require("mini.comment").setup()
+	require("mini.pick").setup()
+	require("mini.surround").setup()
+	require("mini.bufremove").setup()
+	-- require("mini.indentscope").setup()
+	require("mini.visits").setup()
+	require("mini.diff").setup()
+
+	local hipatterns = require("mini.hipatterns")
+	hipatterns.setup({
+		highlighters = {
+			-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+			fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+			hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+			todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+			note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+			-- Highlight hex color strings (`#rrggbb`) using that color
+			hex_color = hipatterns.gen_highlighter.hex_color(),
+		},
+	})
 end)
