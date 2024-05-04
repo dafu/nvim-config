3ed9f3904ba38aa78d7f3521dca65941ffd81445
daniel fusser <daniel.fusser@gmail.com>
2024-05-05 00:06:31 +0200

    add lsp keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -203,6 +203,29 @@ now(function()
 				vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 			end
 
+			-- Displays hover information about the symbol under the cursor
+			map("K", "<cmd>lua vim.lsp.buf.hover()<cr>", "")
+			-- Jump to the definition
+			map("gd", "<cmd>lua vim.lsp.buf.definition()<cr>", "")
+			-- Jump to declaration
+			map("gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", "")
+			-- Lists all the implementations for the symbol under the cursor
+			map("gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", "")
+			-- Jumps to the definition of the type symbol
+			map("go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", "")
+			-- Lists all the references
+			map("gr", "<cmd>lua vim.lsp.buf.references()<cr>", "")
+			-- Displays a function's signature information
+			map("gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", "")
+			-- Renames all references to the symbol under the cursor
+			map("<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", "")
+			-- Selects a code action available at the current cursor position
+			map("<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", "")
+			-- Show diagnostics in a floating window
+			map("gl", "<cmd>lua vim.diagnostic.open_float()<cr>", "")
+			-- Move to the previous diagnostic
+			map("[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "")
+
 			-- Jump to the definition of the word under your cursor.
 			--  This is where a variable was first declared, or where a function is defined, etc.
 			--  To jump back, press <C-T>.
