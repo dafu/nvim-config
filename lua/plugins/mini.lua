296ee8cb7a58689ba275e2ee76c07031729f2637
daniel fusser <daniel.fusser@gmail.com>
2024-05-14 23:52:41 +0200

    change statusbar

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -36,7 +36,55 @@ now(function()
 	vim.notify = require("mini.notify").make_notify()
 
 	require("mini.tabline").setup()
-	require("mini.files").setup()
+	require("mini.files").setup({
+		-- Customization of shown content
+		content = {
+			-- Predicate for which file system entries to show
+			filter = nil,
+			-- What prefix to show to the left of file system entry
+			prefix = nil,
+			-- In which order to show file system entries
+			sort = nil,
+		},
+
+		-- Module mappings created only inside explorer.
+		-- Use `''` (empty string) to not create one.
+		mappings = {
+			close = "q",
+			go_in = "l",
+			go_in_plus = "L",
+			go_out = "h",
+			go_out_plus = "H",
+			reset = "<BS>",
+			reveal_cwd = "@",
+			show_help = "g?",
+			synchronize = "=",
+			trim_left = "<",
+			trim_right = ">",
+		},
+
+		-- General options
+		options = {
+			-- Whether to delete permanently or move into module-specific trash
+			permanent_delete = true,
+			-- Whether to use for editing directories
+			use_as_default_explorer = true,
+		},
+
+		-- Customization of explorer windows
+		windows = {
+			-- Maximum number of windows to show side by side
+			max_number = math.huge,
+			-- Whether to show preview of file/directory under cursor
+			preview = true,
+			-- Width of focused window
+			width_focus = 50,
+			-- Width of non-focused window
+			width_nofocus = 15,
+			-- Width of preview window
+			width_preview = 25,
+		},
+	})
 
 	-- vim.o.cmdheight = 0
 	-- vim.o.laststatus = 3
@@ -49,25 +97,25 @@ now(function()
 			active = function()
 				local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
 				local git = MiniStatusline.section_git({ trunc_width = 75 })
-				local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+				-- local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
 				local filename = MiniStatusline.section_filename({ trunc_width = 140 })
 				local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-				local location = MiniStatusline.section_location({ trunc_width = 75 })
+				-- local location = MiniStatusline.section_location({ trunc_width = 75 })
 				local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
 				return MiniStatusline.combine_groups({
 					{ hl = mode_hl, strings = { mode } },
-					{ hl = "MiniStatuslineFilename", strings = { git, diagnostics } },
+					{ hl = "MiniStatuslineFilename", strings = { git } }, --, diagnostics } },
 					"%<", -- Mark general truncate point
 					{ hl = "MiniStatuslineFilename", strings = { filename } },
 					"%=", -- End left alignment
 					{ hl = "MiniStatuslineFilename", strings = { fileinfo } },
-					{ hl = "MiniStatuslineFilename", strings = { search, location } },
+					{ hl = "MiniStatuslineFilename", strings = { search } }, -- location
 				})
 			end,
 		},
 	})
-	require("mini.starter").setup()
+	-- require("mini.starter").setup()
 	require("mini.ai").setup()
 	require("mini.comment").setup()
 	require("mini.pick").setup()
