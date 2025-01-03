ef3cbb89eed9ea5c52f1a8ff4c9d6b2484188cb7
daniel fusser <daniel.fusser@gmail.com>
2025-01-03 01:07:18 +0100

    change markdown plugin

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,5 +1,10 @@
 local MiniDeps = require("mini.deps")
 
 MiniDeps.add({
-	source = "MeanderingProgrammer/render-markdown.nvim",
+	source = "OXY2DEV/markview.nvim",
+
+	depends = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
 })
