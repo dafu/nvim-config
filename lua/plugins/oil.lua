86f85ac9e52aac9fe27cfbc62bd032a8550c8003
dafu <daniel.fusser@gmail.com>
2025-01-26 13:14:47 +0100

    oil: extend config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,28 @@ MiniDeps.now(function()
 	MiniDeps.add("stevearc/oil.nvim")
 	require("oil").setup({
 		default_file_explorer = true,
+		delete_to_trash = true,
+		skip_confirm_for_simple_edits = true,
+		view_options = {
+			show_hidden = true,
+			natural_order = true,
+			is_always_hidden = function(name, _)
+				return name == ".." or name == ".git"
+			end,
+		},
+		float = {
+			padding = 2,
+			max_width = 90,
+			max_height = 0,
+		},
+		win_options = {
+			wrap = true,
+			winblend = 0,
+		},
+		keymaps = {
+			["<C-c>"] = "actions.close",
+			["q"] = "actions.close",
+		},
 	})
 	vim.keymap.set("n", "-", "<CMD>Oil<CR>")
 end)
