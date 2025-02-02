6caf8bb23a9a6ff3c53d12274bf2364c7e041fba
daniel fusser <daniel.fusser@gmail.com>
2025-02-03 00:51:02 +0100

    md: switch back to oxyde for checkbox functionality

--- lua/plugins/md.lua
+++ lua/plugins/md.lua
@@ -1,5 +1,21 @@
 MiniDeps.now(function()
 	MiniDeps.add({
-		source = "MeanderingProgrammer/render-markdown.nvim",
+		source = "OXY2DEV/markview.nvim",
+		depends = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
 	})
+	local presets = require("markview.presets")
+
+	require("markview").setup({
+		preview = {
+			icon_provider = "mini", -- "internal", -- "mini" or "devicons"
+		},
+		markdown = {
+			headings = presets.headings.glow,
+		},
+	})
+
+	require("markview.extras.checkboxes").setup()
 end)
