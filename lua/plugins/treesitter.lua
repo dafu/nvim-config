822e49314e0a80371d8037d98e7d7613c6584b90
dafu <daniel.fusser@gmail.com>
2025-11-20 22:25:53 +0100

    switch treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,7 @@
 MiniDeps.now(function()
 	MiniDeps.add({
 		source = "nvim-treesitter/nvim-treesitter",
-		-- Use 'master' while monitoring updates in 'main'
-		checkout = "master",
-		monitor = "main",
+		checkout = "main",
 		-- Perform action after every checkout
 		hooks = {
 			post_checkout = function()
@@ -11,25 +9,25 @@ MiniDeps.now(function()
 			end,
 		},
 	})
-	require("nvim-treesitter.configs").setup({
-		modules = {},
-		sync_install = true,
-		ignore_install = {},
-		auto_install = false,
-		ensure_installed = {},
-		-- ensure_installed = { "lua", "vimdoc", "go", "html", "templ", "json" },
-		highlight = {
-			enable = true,
-			-- disable = { "c", "rust" },
-			-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-			disable = function(lang, buf)
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-	})
+	--require("nvim-treesitter.configs").setup({
+	--modules = {},
+	--sync_install = true,
+	--ignore_install = {},
+	--auto_install = false,
+	--ensure_installed = {},
+	---- ensure_installed = { "lua", "vimdoc", "go", "html", "templ", "json" },
+	--highlight = {
+	--enable = true,
+	---- disable = { "c", "rust" },
+	---- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+	--disable = function(lang, buf)
+	--local max_filesize = 100 * 1024 -- 100 KB
+	--local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	--if ok and stats and stats.size > max_filesize then
+	--return true
+	--end
+	--end,
+	--additional_vim_regex_highlighting = false,
+	--},
+	--})
 end)
