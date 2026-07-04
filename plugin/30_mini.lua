8021c8e834d497e1a850eef71c88866db3f2277a
daniel fusser <daniel.fusser@provaris.com>
2026-07-04 15:54:18 +0200

    disable starter

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -56,29 +56,29 @@ now(function()
 end)
 
 -- Starter screen: must be available before the first buffer is shown.
-now(function()
-  local starter = require("mini.starter")
-  starter.setup({
-    evaluate_single = true,
-    header = [[
-██████╗  █████╗ ███████╗██╗   ██╗   ██████╗ ███████╗██╗   ██╗
-██╔══██╗██╔══██╗██╔════╝██║   ██║   ██╔══██╗██╔════╝██║   ██║
-██║  ██║███████║█████╗  ██║   ██║   ██║  ██║█████╗  ██║   ██║
-██║  ██║██╔══██║██╔══╝  ██║   ██║   ██║  ██║██╔══╝  ╚██╗ ██╔╝
-██████╔╝██║  ██║██║     ╚██████╔╝██╗██████╔╝███████╗ ╚████╔╝
-╚═════╝ ╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝╚═════╝ ╚══════╝  ╚═══╝  ]],
-    footer = "",
-    items = {
-      starter.sections.recent_files(10, false),
-      starter.sections.recent_files(5, true),
-      starter.sections.sessions(5, true),
-    },
-    content_hooks = {
-      starter.gen_hook.adding_bullet(),
-      starter.gen_hook.aligning("center", "center"),
-    },
-  })
-end)
+-- now(function()
+--   local starter = require("mini.starter")
+--   starter.setup({
+--     evaluate_single = true,
+--     header = [[
+-- ██████╗  █████╗ ███████╗██╗   ██╗   ██████╗ ███████╗██╗   ██╗
+-- ██╔══██╗██╔══██╗██╔════╝██║   ██║   ██╔══██╗██╔════╝██║   ██║
+-- ██║  ██║███████║█████╗  ██║   ██║   ██║  ██║█████╗  ██║   ██║
+-- ██║  ██║██╔══██║██╔══╝  ██║   ██║   ██║  ██║██╔══╝  ╚██╗ ██╔╝
+-- ██████╔╝██║  ██║██║     ╚██████╔╝██╗██████╔╝███████╗ ╚████╔╝
+-- ╚═════╝ ╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝╚═════╝ ╚══════╝  ╚═══╝  ]],
+--     footer = "",
+--     items = {
+--       starter.sections.recent_files(10, false),
+--       starter.sections.recent_files(5, true),
+--       starter.sections.sessions(5, true),
+--     },
+--     content_hooks = {
+--       starter.gen_hook.adding_bullet(),
+--       starter.gen_hook.aligning("center", "center"),
+--     },
+--   })
+-- end)
 
 -- NOW IF ARGS =================================================================
 
